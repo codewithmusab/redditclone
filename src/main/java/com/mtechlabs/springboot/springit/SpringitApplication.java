c6322881b8171509fc0d3a18e31baa836edb484e
m.sesay@ubuntuafrika.com <m.sesay@ubuntuafrika.com>
2020-04-16 17:00:16 +0000

    UPDATE Database

--- src/main/java/com/mtechlabs/springboot/springit/SpringitApplication.java
+++ src/main/java/com/mtechlabs/springboot/springit/SpringitApplication.java
@@ -1,13 +1,36 @@
 package com.mtechlabs.springboot.springit;
 
+import com.mtechlabs.springboot.springit.model.domain.Comment;
+import com.mtechlabs.springboot.springit.model.domain.Link;
+import com.mtechlabs.springboot.springit.model.domain.doa.CommentRepository;
+import com.mtechlabs.springboot.springit.model.domain.doa.LinkRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
+//        return args -> {
+//            Link link = new Link("Getting Started with Spring Boot 2","https://therealdanvega.com/spring-boot-2");
+//            linkRepository.save( link );
+//
+//            Comment comment = new Comment("This Spring Boot 2 Link is awesome",link);
+//            commentRepository.save(comment);
+//            link.addComment(comment);
+//
+//            System.out.println(link.getComments());
+//        };
+//    }
+
 
 }
--- src/main/java/com/mtechlabs/springboot/springit/model/domain/Link.java
+++ src/main/java/com/mtechlabs/springboot/springit/model/domain/Link.java
@@ -3,6 +3,8 @@ package com.mtechlabs.springboot.springit.model.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
 
 @Entity
 public class Link extends Auditable {
@@ -14,4 +16,7 @@ public class Link extends Auditable {
     private String url;
 
     // comments
+
+//    @OneToMany(mappedBy = "link")
+//    private Link<Comment> comments=new ArrayList<>();
 }
